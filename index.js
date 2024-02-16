80dbf732bbe5fe4e0c1584893a9f4ad742f31d51
KL <karl.w.langer@gmail.com>
2024-02-17 09:38:38 +1030

    Linked all modules with index.js to run smoothly

--- index.js
+++ index.js
@@ -2,18 +2,33 @@
 //Import an SVG image class 
 const promptUser = require("./lib/handleInput")
 const writeFile = require('./lib/writeFile');
-const Circle = require('./lib/shapes')
+const {Circle, Square, Triangle} = require('./lib/shapes')
+const Logo = require('./lib/generateImageMarkdown')
 
 
 
 //Ask for user prompts
-let results = promptUser();
 
-// Wait for user to complete prompts 
+
+promptUser()
+    .then((response) => {
+        console.log(response)
+        let logo = new Logo(response)
+        logo.constructShape(logo.shape)
+        console.log(logo)
+        writeFile(logo.generateMarkdown());
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+    
+    
+function fetchShape(){
+
+}
 
 // Generate desired shapes
 
 //Generate markup
 
 //generate file 
-writeFile(content);
