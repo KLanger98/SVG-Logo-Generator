588d57c12e006a5baa897a1fce75e190637bb5fe
KL <karl.w.langer@gmail.com>
2024-02-16 15:48:36 +1030

    Added basic outline for functions before adding Tests

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//Import an SVG image class 
+const promptUser = require("./lib/handleInput")
+const writeFile = require('./lib/writeFile');
+const Circle = require('./lib/shapes')
+
+
+
+//Ask for user prompts
+
+// Wait for user to complete prompts 
+
+// Generate desired shapes
+
+//Generate markup
+
+//generate file 
+writeFile(content);
